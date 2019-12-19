cbe60488bbf52d74645a6deb3db09818166e2fb7
sebmat30 <kseb57290@hotmail.com>
2019-12-19 16:34:29 +0100

    commit sa marche pas encore

--- scrap/checkurl.py
+++ scrap/checkurl.py
@@ -1,4 +1,8 @@
 from pip._vendor import requests
+import json
+import os
+from bs4 import BeautifulSoup
+
 # html = requests.get("https://www.liberation.fr/")
 # print("Statut :", html.status_code)
 # print("Headers :", html.headers)
@@ -7,6 +11,62 @@ from pip._vendor import requests
 # print("Statut :", html.status_code)
 # print("Headers :", html.headers)
 # print("Text :", html.text)
+dataset = []
+
+
+def printseparator():
+    """ Fonction qui affiche une ligne de séparation """
+    print("-" * 50)
+
+
+F_URL = "url"
+F_STATUS = "status_code"
+F_HTML = "content"
+F_TITLE = "title"
+
+
+def writetodict(html, is_verbose):
+    title = search_title(html.text)
+    dict = {F_URL: html.url, F_STATUS: html.status_code, F_HTML: html.text[:1000], F_TITLE: title}
+    global dataset 
+    dataset.append(dict)
+
+    # affiche le nom du fichier .py
+    print(__file__)
+    # affiche le repertoire absolue pour le système d'exploitation
+    print(os.path.abspath(__file__))
+    # affiche le repertoire contenant le fichier .py
+    print(os.path.dirname(__file__))
+    # recupere le nom du fichier dans la configuration du systeme d'exploitation
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    print(basedir)
+    # creation du fichier chekurl.json dans le repertoire scrap
+    filename = basedir + "/" + "checkurl.json"
+    with open(filename, "w", encoding="utf8") as f:
+        json.dump(dataset, f)
+        print(f"file {filename} created !")
+
+
+def search_title_by_bs4(text):
+    soup = BeautifulSoup(text)
+    print(soup.title.string)
+    return soup.title.String
+
+
+def search_title(text):  
+    """ fonction qui cherche le titre d'une page HTML """ 
+    return search_title_by_bs4(text)
+    retbuffer = begin = 0
+    end = None
+    begin = text.find("<title>")
+    if begin != -1:
+        begin += len("<title>")
+        end = text[begin:].find("</title>")
+        if end != -1:
+            end += begin
+            retbuffer = text[begin:end]
+    print(f"Test search_title: {begin}, {end}, {retbuffer}")
+    return retbuffer
 
 
 def get(url):
@@ -17,7 +77,8 @@ def get(url):
     return html
 
 
-def get_urls(arglist, is_verbose=False):
+def get_urls(arglist, is_verbose=True):
+    """ recuperer tout les urls listés dans listedesurls[ ] """
     for url_en_arg in arglist:
         try:
             html = get(url_en_arg)
@@ -26,26 +87,40 @@ def get_urls(arglist, is_verbose=False):
             print(str(e))
             html = None
         if html:
-            displayurl(html)
+            displayurl(html, is_verbose)
+            writetodict(html, is_verbose)
+            
 
-
-def displayurl(html, is_verbose=False):
+def displayurl(html, is_verbose):
     print(f"--> Il y a {len(html.text)} octets dans {html.url}")
     if is_verbose:
+        printseparator()
         print("Statut :", html.status_code)
+        printseparator()
         print("Headers :", html.headers)
-        print("Text :", html.text)
-        for key, value in html.headers:
+        printseparator()
+        # print("Text :", html.text)
+    else:
+        print(f"Erreur de request vers {html.url} ---- avec code : {html.status_code}")
+        for key, value in html.headers.items():
             print(f"{key} : {value}")
+    # def writetofile(html):
+    #     file = open("monfile.txt", "w")
+    #     file.write(f"Url : {F_URL} Code : {F_STATUS} Texte : {F_HTML}")
+    #     file.close()
 
-
-if __name__ == "__main__":
-    listedesurls = ["matin", "midi", "soir", "minuit", "aube"]
-    for item in listedesurls:
+    if __name__ == "__main__":
+        letempsquipasse = ["matin", "midi", "soir", "minuit", "aube"]
+    for item in letempsquipasse:
         print(item)
     listedesurls = [
-        "http://localhost:8080/formext/avions/avions",
-        "https://www.dealabs.com/nouveaux",
-        "https://github.com/Bebounet/babel-bebounet/blob/develop-bebounet/.gitignore",
+        # "http://localhost:8080/formext/avions/avions",
+        "https://www.dealabs.com",
+        "https://www.ouest-france.fr/",
     ]
     get_urls(listedesurls)
+
+    
+print(len(dataset))
+with open("test.json", "w+", encoding="utf8") as f:
+    json.dump(dataset, f)
